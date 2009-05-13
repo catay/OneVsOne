e6af7ae9b5f1c9de95e362566e78c0958da18faa
Steven Mertens <steven.mertens@catay.be>
2009-05-13 23:45:20 +0200

    Added the the report_matches.sh bash script that can be run in crontab to send the match results by e-mail. Failback system for the league.

---
+++ examples/report_matches.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#
+# !! ADJUST THE UPPERCASE VARIABLES TO YOUR PREFERENCES !!
+#
+
+LOGFILES="/var/tmp/classic.log /var/tmp/hix.log /var/tmp/fancy.log"
+
+CC="some_other_recipient@example.com"
+FROM="noreply@example.com"
+TO="1vs1@bzleague.com"
+
+# get yesterdays date
+yesterday=$(date --utc -d '1 day ago' +"%Y-%m-%d")
+
+if [[ $(sort ${LOGFILES} | grep -c "${yesterday}") -ne 0 ]]
+then
+	sort ${LOGFILES} | grep "${yesterday}" |
+	mail -a "From: ${FROM}" -s 1vs1MatchSorted -c ${CC} ${TO}
+fi
+
+exit 0
